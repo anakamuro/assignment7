1dbd9f0e6f44206116333f35d7bb6ee18f85f79f
Akihiko Nakamura <anakamuro@earthlink.net>
2022-06-30 22:18:18 -0400

    Update fetchData.js

--- js/fetchData.js
+++ js/fetchData.js
@@ -473,21 +473,6 @@ function selUstensils() {
 
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-function linearSearch(arr, elem) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === elem) {
-      return i;
-    }
-  }
-  return -1;
-}
-
-console.log(linearSearch(arr, -5));
-
-// ingredientsTags
-// appliancesTags
-// ustensTags
-
 const filterAll = async (searchBox) => {
   searchBox = allTags;
   document.getElementById("recipeList").innerHTML = "";
